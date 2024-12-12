4e068cde664d34ed6281ed04284726be9461b9ac
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-12 17:22:06 +0530

    Create HeroSection.js

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-overlay">
+        <div className="Left_box">
+          <div className="upper_text_box">
+            <div className="welcome_box">
+              <div className="box1">
+              </div>
+              <div className="box2">
+              </div>
+              <div className="box3">
+              </div>
+            </div>
+            <div className="text_box">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
