c2b374e2a1095fd05746ad472f781ec2c739dbb7
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-12 17:20:33 +0530

    Create GallerySection.js

---
+++ src/components/GallerySection/GallerySection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./GallerySection.css";
+
+const GallerySection = () => {
+  return (
+    <section className="gallery-section">
+      <div className='gal_box' id='left_side'>
+      </div>
+      <div className="gal_box">
+        <h1>Treasure Hunt</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur expedita, id natus iure aliquam rerum quibusdam laboriosam quis distinctio aut.  Sed quas pariatur dolor ea modi voluptates quasi praesentium a. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur expedita, id natus iure aliquam rerum quibusdam laboriosam quis distinctio aut.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur expedita, id natus iure aliquam rerum quibusdam laboriosam quis distinctio aut.</p>
+        <div className="button_box">
+          <h1>Read More -></h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GallerySection;
