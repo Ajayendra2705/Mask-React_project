78b8006ff3e5a5d4a16015a6db08b958ba7908c4
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-12 17:18:35 +0530

    Create EventsSection.js

---
+++ src/components/EventsSection/EventsSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./EventsSection.css";
+
+const EventsSection = () => {
+  return (
+    <section className="events-section">
+      <div className="big_box">
+        <div className="recent_box">
+
+        </div>
+        <div className="box">
+
+        </div>
+        <div className="box">
+
+        </div>
+        <div className="box">
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventsSection;
