fe6fc5064f6f8f92f9be91cafbe9e9409c21ef9b
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-12 19:49:45 +0530

    Create Teams.js

---
+++ src/components/Teams/Teams.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Teams.css";
+
+const Teams = () => {
+  return (
+    <section className="teams">
+      <div className="outer_box">
+        <div className="left_side">
+            <div className="team_text_box">
+
+            </div>
+        </div>
+        <div className="right_side">
+            <div className="box_side_2">
+                <div className="team_box_1">
+
+                </div>
+                <div className="team_box_1">
+
+                </div>
+            </div>
+            <div className="box_side_3">
+                <div className="team_box_2">
+
+                </div>
+                <div className="team_box_2">
+
+                </div>
+                <div className="team_box_2">
+
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Teams;
