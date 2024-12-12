d5f183029e2bb27af4347fd5923f64051f0978b7
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-12 17:10:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import EventsSection from "./components/EventsSection/EventsSection";
+import GallerySection from "./components/GallerySection/GallerySection";
+import Carousel from "./components/Carousel/Carousel";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+      <EventsSection />
+      <GallerySection />
+      <Carousel />
+      <AboutUs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
